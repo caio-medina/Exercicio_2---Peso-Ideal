a78395bbf404807e04bd1522661a6d43f9987182
Caio Medina <caio__medina@hotmail.com>
2017-06-13 19:57:00 -0300

    Distincao entre homem(k=4) e mulher(k=2) & formula implementada

--- app/src/main/java/a2017_1/pooa/iff/br/pesoideal/MainActivity.java
+++ app/src/main/java/a2017_1/pooa/iff/br/pesoideal/MainActivity.java
@@ -2,12 +2,48 @@ package a2017_1.pooa.iff.br.pesoideal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+    private RadioButton homem = (RadioButton) findViewById(R.id.rdbHomem);
+    private RadioButton mulher = (RadioButton) findViewById(R.id.rdbMulher);
+    private EditText altura;
+    private Button calcular = (Button) findViewById(R.id.btCalcular);
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        altura = (EditText) findViewById(R.id.edtAltura);
+
+
+        calcular.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int k;
+                float p;
+                float ialtura = Float.parseFloat(altura.getText().toString());
+                if (homem.isChecked() == true) {
+                    k=4;
+                    p= (ialtura-100) - (ialtura-150)/k;
+
+                }
+                if (mulher.isChecked() == true) {
+                    k=2;
+                    p= (ialtura-100) - (ialtura-150)/k;
+
+                }
+
+            }
+        });
     }
+
+
 }
+
+
