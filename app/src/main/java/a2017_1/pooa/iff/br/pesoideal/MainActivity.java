84bb17ee95a96e21695a2236cbda7bd0513a2206
Caio Medina <caio__medina@hotmail.com>
2017-06-13 23:38:59 -0300

    Implementado a formula & figura do homem e mulher com condicoes (erro)

--- app/src/main/java/a2017_1/pooa/iff/br/pesoideal/MainActivity.java
+++ app/src/main/java/a2017_1/pooa/iff/br/pesoideal/MainActivity.java
@@ -5,22 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private RadioButton homem = (RadioButton) findViewById(R.id.rdbHomem);
-    private RadioButton mulher = (RadioButton) findViewById(R.id.rdbMulher);
+    final RadioButton homem = (RadioButton) findViewById(R.id.rdbHomem);
+    final RadioButton mulher = (RadioButton) findViewById(R.id.rdbMulher);
     private EditText altura;
     private Button calcular = (Button) findViewById(R.id.btCalcular);
-
+    private ImageView imagem_homem,imagem_mulher;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         altura = (EditText) findViewById(R.id.edtAltura);
-
+        imagem_homem = (ImageView) findViewById(R.id.imgView);
+        imagem_mulher = (ImageView) findViewById(R.id.imgView);
 
         calcular.setOnClickListener (new View.OnClickListener() {
             @Override
@@ -31,12 +33,13 @@ public class MainActivity extends AppCompatActivity {
                 if (homem.isChecked() == true) {
                     k=4;
                     p= (ialtura-100) - (ialtura-150)/k;
+                    imagem_homem.setImageResource(R.drawable.homem);
 
                 }
                 if (mulher.isChecked() == true) {
                     k=2;
                     p= (ialtura-100) - (ialtura-150)/k;
-
+                    imagem_mulher.setImageResource(R.drawable.mulher);
                 }
 
             }
